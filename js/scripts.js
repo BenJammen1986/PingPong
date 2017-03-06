d9fa5078bbd691540c16d44438fe1b2a57c218b1
Ben <benschenkenberger@gmail.com>
2017-03-06 07:00:15 -0800

    Specs 1-3 passed: output for multiples of 3,5, and 15 are correct.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,52 @@
+
+
+
+// Description:
+// - The users inputted number is taken and parsed, value collected.
+// - We need one "for" loop to run through and output numbers one at a time until the index value = whatever number the user inputs.
+// - Each pass (each number) is saved into a variable and outputted to the .results section.
+// - If the # saved in the variable is % 3 === 0: then save "ping" to variable and output this string.
+// - If the # saved in the variable is % 5 === 0: then save "pong" to the variable and output this string.
+// - If the # saved in the variable is % 15 === 0: then save "ping-pong" to the variable and output this string.
+//
+// - The loop will end when it hits the number inputted by the user.
+
+// Business logic:
+
+
+
+var pingPong = function(userInput) {
+	if (userInput % 15 === 0) {
+		return "Ping-Pong";
+    }
+	else if (userInput % 3 === 0) {
+		return "Ping";
+    }
+	else if (userInput % 5 === 0) {
+		return "Pong";
+    }
+	else {
+		return userInput;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+// User interface logic:
+
 $(document).ready(function() {
   $("#numberForm").submit(function(event) {
       event.preventDefault();
       var userInput = parseInt($("input#numberInput").val());
+      var output = pingPong(userInput);
 
 
 
@@ -12,8 +57,60 @@ $(document).ready(function() {
       //   rejoinCharacterArray (inputArray);
       // };
 
-      $("#results").append("<li>" + userInput + "</li>");
+      $("#results").append("<li>" + output + "</li>");
       $(".results").show();
 
   });
 });
+
+
+
+
+
+// var pingPong = function(userInput) {
+// 	if (userInput % 15 === 0) {
+// 		return "Ping-Pong";
+//     }
+// 	else if (userInput % 3 === 0) {
+// 		return "Ping";
+//     }
+// 	else if (userInput % 5 === 0) {
+// 		return "Pong";
+//     }
+// 	else {
+// 		return userInput;
+//     }
+// };
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // User interface logic:
+//
+// $(document).ready(function() {
+//   $("#numberForm").submit(function(event) {
+//       event.preventDefault();
+//       var userInput = parseInt($("input#numberInput").val());
+//       var output = pingPong(userInput);
+//
+//
+//
+//       // makeIndWordArrays (englishInput);
+//       // for (var b=0;b<masterArray.length;b++) {
+//       //   makeIndCharacterArray (masterArray[b]);
+//       //   translateString (inputArray);
+//       //   rejoinCharacterArray (inputArray);
+//       // };
+//
+//       $("#results").append("<li>" + output + "</li>");
+//       $(".results").show();
+//
+//   });
+// });
